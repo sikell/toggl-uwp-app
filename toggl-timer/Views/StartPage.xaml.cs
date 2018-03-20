4d5728c38ceb8b3e8b4c23a52960ee0d7aaabe3c
Simon Keller <simonkeller94@aol.com>
2018-03-20 23:08:09 +0100

    Initialize prism project structure

--- toggl-timer/Views/StartPage.xaml.cs
+++ toggl-timer/Views/StartPage.xaml.cs
@@ -15,14 +15,14 @@ using Windows.UI.Xaml.Navigation;
 
 // Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
 
-namespace toggl_timer
+namespace toggl_timer.Views
 {
     /// <summary>
     /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class StartPage : Page
     {
-        public MainPage()
+        public StartPage()
         {
             this.InitializeComponent();
         }
