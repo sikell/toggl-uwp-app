616349ab902147b7ee944f5654984deeb0a28932
Simon Keller <simonkeller94@aol.com>
2018-03-23 18:48:26 +0100

    Refactoring of navigation view

--- Controls/PageHeader.xaml.cs
+++ Controls/PageHeader.xaml.cs
@@ -2,8 +2,6 @@
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
-
 namespace TogglTimer.Controls
 {
     public sealed partial class PageHeader : UserControl
@@ -15,23 +13,24 @@ namespace TogglTimer.Controls
             this.Loaded += (s, a) =>
             {
                 AppShell.Current.TogglePaneButtonRectChanged += Current_TogglePaneButtonSizeChanged;
-                this.titleBar.Margin = new Thickness(AppShell.Current.TogglePaneButtonRect.Right, 0, 0, 0);
+                this.TitleBar.Margin = new Thickness(AppShell.Current.TogglePaneButtonRect.Right, 0, 0, 0);
             };
         }
 
         private void Current_TogglePaneButtonSizeChanged(AppShell sender, Rect e)
         {
-            this.titleBar.Margin = new Thickness(e.Right, 0, 0, 0);
+            this.TitleBar.Margin = new Thickness(e.Right, 0, 0, 0);
         }
 
         public UIElement HeaderContent
         {
-            get { return (UIElement)GetValue(HeaderContentProperty); }
-            set { SetValue(HeaderContentProperty, value); }
+            get => (UIElement) GetValue(HeaderContentProperty);
+            set => SetValue(HeaderContentProperty, value);
         }
 
         // Using a DependencyProperty as the backing store for HeaderContent.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty HeaderContentProperty =
-            DependencyProperty.Register("HeaderContent", typeof(UIElement), typeof(PageHeader), new PropertyMetadata(DependencyProperty.UnsetValue));
+            DependencyProperty.Register("HeaderContent", typeof(UIElement), typeof(PageHeader),
+                new PropertyMetadata(DependencyProperty.UnsetValue));
     }
 }
