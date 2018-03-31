797efd9adec23064e83b4f337a9b0584658912b4
Simon Keller <simonkeller94@aol.com>
2018-03-31 12:25:29 +0200

    Extend abstract api client in api client impl

--- Services/Api/ApiClient.cs
+++ Services/Api/ApiClient.cs
@@ -12,23 +12,15 @@ using TogglTimer.Services.Api.Model;
 
 namespace TogglTimer.Services.Api
 {
-    public class ApiClient : IApiClient
+    public class ApiClient : AbstractApiClient, IApiClient
     {
-        private readonly HttpClient _client;
         private readonly ILogger _log = LogManagerFactory.DefaultLogManager.GetLogger<ApiClient>();
 
-        public ApiClient()
+        public ApiClient(HttpClient client) : base(client)
         {
-            var handler = new HttpClientHandler
-            {
-                AllowAutoRedirect = true,
-            };
-            _client = new HttpClient(handler)
-            {
-                BaseAddress = new Uri("https://www.toggl.com/api/v8/")
-            };
-            _client.DefaultRequestHeaders.Accept.Clear();
-            _client.DefaultRequestHeaders.Accept.Add(
+            client.BaseAddress = new Uri("https://www.toggl.com/api/v8/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
@@ -76,87 +68,5 @@ namespace TogglTimer.Services.Api
             return (await DoGet<DataWrapperDto<ProjectDto>>("projects/" + projectId, ToBasicAuth(apiToken))).data;
         }
 
-        private async Task<TResult> DoPut<TResult, TBody>(string url, string credentials, TBody body)
-        {
-            var payload = JsonConvert.SerializeObject(body);
-            _log.Debug("PUT {0} - {1}", url, payload);
-            var request = new HttpRequestMessage
-            {
-                RequestUri = new Uri(_client.BaseAddress + url),
-                Method = HttpMethod.Put,
-                Headers =
-                {
-                    {HttpRequestHeader.Authorization.ToString(), "Basic " + credentials},
-                    {HttpRequestHeader.ContentType.ToString(), "application/json"}
-                },
-                Content = body != null ? new StringContent(payload) : null
-            };
-
-            var response = await _client.SendAsync(request);
-            if (!response.IsSuccessStatusCode)
-            {
-                _log.Error("PUT {0} failed with {1}!", url, response.StatusCode);
-                return default(TResult);
-            }
-
-            var readAsStringAsync = await response.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<TResult>(readAsStringAsync);
-        }
-
-        private async Task<TResult> DoPost<TResult, TBody>(string url, string credentials, TBody body)
-        {
-            var payload = JsonConvert.SerializeObject(body);
-            _log.Debug("POST {0} - {1}", url, payload);
-            var request = new HttpRequestMessage
-            {
-                RequestUri = new Uri(_client.BaseAddress + url),
-                Method = HttpMethod.Post,
-                Headers =
-                {
-                    {HttpRequestHeader.Authorization.ToString(), "Basic " + credentials},
-                    {HttpRequestHeader.ContentType.ToString(), "application/json"}
-                },
-                Content = body != null ? new StringContent(payload) : null
-            };
-
-            var response = await _client.SendAsync(request);
-            if (!response.IsSuccessStatusCode)
-            {
-                _log.Error("POST {0} failed with {1} - {2}! ", url, response.StatusCode, await response.Content.ReadAsStringAsync());
-                return default(TResult);
-            }
-
-            var readAsStringAsync = await response.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<TResult>(readAsStringAsync);
-        }
-
-        private async Task<T> DoGet<T>(string url, string credentials)
-        {
-            _log.Debug("GET {0}", url);
-            var request = new HttpRequestMessage
-            {
-                RequestUri = new Uri(_client.BaseAddress + url),
-                Method = HttpMethod.Get,
-                Headers =
-                {
-                    {HttpRequestHeader.Authorization.ToString(), "Basic " + credentials}
-                }
-            };
-
-            var response = await _client.SendAsync(request);
-            if (!response.IsSuccessStatusCode)
-            {
-                _log.Error("GET {0} failed with {1}! ", url, response.StatusCode);
-                return default(T);
-            }
-
-            var readAsStringAsync = await response.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<T>(readAsStringAsync);
-        }
-
-        private static string ToBasicAuth(string apiToken)
-        {
-            return Convert.ToBase64String(Encoding.ASCII.GetBytes(apiToken + ":api_token"));
-        }
     }
 }
